cd7a4b1e715c67d2de7abb67c8e0a3c05ecb9f33
Peter Shrader <pshrader@bsd220.org>
2022-02-01 10:22:31 -0600

    Initial Commit

---
+++ API Calling Hackwhich/API_Calling_HackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_HackwhichApp.swift
+//  API Calling Hackwhich
+//
+//  Created by Student on 2/1/22.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_HackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
